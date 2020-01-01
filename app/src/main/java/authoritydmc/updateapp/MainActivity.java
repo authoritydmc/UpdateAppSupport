15339e1a2d4a61dceee3288b710f4c0dc261cb2b
Raj dubey <com.mailuser@gmail.com>
2020-01-01 16:26:23 +0530

    Initial Release base

--- app/src/main/java/authoritydmc/updateapp/MainActivity.java
+++ app/src/main/java/authoritydmc/updateapp/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseApp.initializeApp(MainActivity.this);
+      //  FirebaseApp.initializeApp(MainActivity.this);
        // startActivity(new Intent(MainActivity.this,com.vastgk.updateapp.UpdateApp.class));
         UpdateApp.checkupdate(MainActivity.this, BuildConfig.VERSION_NAME,true);
     }
